a1072a003841e91efdbe5e7b6626a1549485f14a
Jason Cobb <jason.cobb@jasoncobb.me>
2024-01-02 12:12:14 -0500

    added vs code settings

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ dot_folders=(
 font_dir="fonts"
 
 brew_packages=(
+    nvim
     stow    
     tmux
 )
@@ -22,16 +23,16 @@ brew_cask_packages=(
     amethyst
     wezterm
 )
-
+echo "[+] - $(date '+%F %T') - Dot File and Configuration Started" 
 # Install Fonts
-echo "Installing Fonts"
+echo "[+] - $(date '+%F %T') - Installing Fonts"
 for file_name in $script_dir/fonts/*
 do
     cp $file_name $HOME/Library/Fonts/
 done
 
 # Install oh-my-zsh
-echo "Installing or Updating oh-my-zsh"
+echo "[+] - $(date '+%F %T') - Installing or Updating oh-my-zsh"
 if ls $HOME/.oh-my-zsh >/dev/null 
 then
     $HOME/.oh-my-zsh/tools/upgrade.sh
@@ -40,7 +41,7 @@ else
 fi
 
 # Install brew
-echo "Installing or Updating brew"
+echo "[+] - $(date '+%F %T') - Installing or Updating brew"
 if brew --version >/dev/null; then
     brew update
 else
@@ -74,45 +75,61 @@ function install_or_upgrade_cask {
     HOMEBREW_NO_AUTO_UPDATE=0
 }
 
-echo "Updating brew packages"
+echo "[+] - $(date '+%F %T') - Updating brew packages"
 for i in ${brew_packages[@]}
 do
     install_or_upgrade $i    
 done
 
-echo "Updating brew cask packages"
+echo "[+] - $(date '+%F %T') - Updating brew cask packages"
 for i in ${brew_cask_packages[@]}
 do
     install_or_upgrade_cask $i
 done
 
-echo "Cleaning up brew"
+echo "[+] - $(date '+%F %T') - Cleaning up brew"
 brew cleanup
 
-echo "Symlinking dotfiles"
+echo "[+] - $(date '+%F %T') - Symlinking dotfiles"
 for folder in ${dot_folders[@]}
 do
-    echo "[+] Folder :: $folder"
+    echo "    [+] - $(date '+%F %T') -  Folder :: $folder"
     stow --ignore=README.md --ignore=LICENSE --ignore=.DS_Store \
         -t $HOME $folder
     # stow -v -t $HiOME $folder
 done
 
-echo "Checking for TPM Installed"
+echo "[+] - $(date '+%F %T') - Checking for TPM Installed"
 if ! ls $HOME/.tmux/plugins/tpm >/dev/null
 then
-    echo "TPM not found, installing"
+    echo "    [+] - $(date '+%F %T') - TPM not found, installing"
     mkdir -p $HOME/.tmux/plugins/tpm
     git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
-    $HOME/.tmux/plugins/tpm/bin/install_plugins
 fi
 
-echo "Checking for Powerlevel10k"
+echo "[+] - $(date '+%F %T') - Installing TMUX Plugins" 
+$HOME/.tmux/plugins/tpm/bin/install_plugins
+
+echo "[+] - $(date '+%F %T') - Checking for Powerlevel10k"
 if ! ls $HOME/.powerlevel10k >/dev/null
 then
-    echo "Powerlevel10k not found, installing"
+    echo "    [+] - $(date '+%F %T') - Powerlevel10k not found, installing"
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
 fi
+
+echo "[+] - $(date '+%F %T') - Checking for VS Code installed"
+if which code >/dev/null
+then
+    echo "    [+] - $(date '+%F %T') - Moving settings.json into place"
+    cp $script_dir/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
+    echo "    [+] - $(date '+%F %T') - Installing VS Code Extensions"
+    for extension in $(cat $script_dir/vscode/extensions.list)
+    do
+        echo "        [+] - $(date '+%F %T') - Installing Extension $extension"
+        code --install-extension $extension >/dev/null
+    done
+fi
+
 # Reload shell once installed
-echo "[+] Reloading shell..."
+echo "[+] - $(date '+%F %T') - Reloading shell..."
 exec $SHELL -l
