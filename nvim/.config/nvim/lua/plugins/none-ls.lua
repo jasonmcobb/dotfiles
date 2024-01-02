2a95eb1d89ec749940995d42e847877c9e980eeb
Jason Cobb <jason.cobb@jasoncobb.me>
2024-01-02 10:56:06 -0500

    first set of dotfiles

---
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,16 @@
+return {
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.diagnostics.eslint_d,
+				null_ls.builtins.formatting.csharpier,
+			},
+		})
+
+	vim.keymap.set("n", "<leader>vfc", vim.lsp.buf.format, {})
+	end,
+}
